0bbcac92e2f9e9147b887b612aeeb584474ee816
GreenShadow <a569027851@gmail.com>
2014-07-30 16:28:34 +0800

    all code

---
+++ AndroidService/DataBase.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace AndroidService
+{
+    public class DataBase
+    {
+        private SqlConnection sqlConnection;
+        private SqlCommand sqlCommand;
+        private SqlDataReader sqlDataReader;
+
+        private string connectionString = @"Data Source=GREENSHADOW-PC\SQLEXPRESS;Initial Catalog=GreenShadow;User ID=sa;Password=a5638491";
+        private string sql = null;
+
+        public DataBase()
+        {
+            if (sqlConnection == null)
+            {
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+            }
+        }
+
+        public void Dispose()
+        {
+            if (sqlConnection != null)
+            {
+                sqlConnection.Close();
+                sqlConnection.Dispose();
+            }
+        }
+
+        /// <returns>所有货物信息</returns>
+        public List<string> AllInfo(int X,int Y)
+        {
+            List<string> list = new List<string>();
+            sql = "select * from NEWS_TABLE";
+            int i;
+            try
+            {
+                sqlCommand = new SqlCommand(sql, sqlConnection);
+                sqlDataReader = sqlCommand.ExecuteReader();
+
+                for (i = 0; i < X; i++)
+                    if (!sqlDataReader.Read())
+                        break;
+                while (sqlDataReader.Read())
+                {
+                    list.Add(sqlDataReader[0].ToString().Trim());
+                    list.Add(sqlDataReader[1].ToString().Trim());
+                    list.Add(sqlDataReader[2].ToString().Trim());
+                    i++;
+                    if (i == Y)
+                        break;
+                }
+
+                if (list.Count < (Y - X)*3)
+                    list.Add(null);
+
+                sqlDataReader.Close();
+                sqlCommand.Dispose();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+            return list;
+        }
+    }
+}
---
+++ AndroidService/Service.asmx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+
+namespace AndroidService
+{
+    /// <summary>
+    /// Service 的摘要说明
+    /// </summary>
+    [WebService(Namespace = "http://GreenShadow.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [SoapDocumentService(RoutingStyle = SoapServiceRoutingStyle.RequestElement)]
+    // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消对下行的注释。
+    // [System.Web.Script.Services.ScriptService]
+    public class Service : System.Web.Services.WebService
+    {
+
+        DataBase DB = new DataBase();
+
+        [WebMethod(Description = "返回所有值")]
+        public List<string> AllInfo(int firstCount, int secondCount)
+        {
+            if (firstCount < 0 || secondCount < 0)
+                return null;
+
+            return DB.AllInfo(firstCount, secondCount);
+        }
+    }
+}
